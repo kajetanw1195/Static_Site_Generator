046feed8d325ef34877b6c893d73317c50e58da7
kajetanw1195 <kajetan11435@gmail.com>
2025-03-20 22:48:39 +0100

    added new child class to HTMLNode: LeafNode, added test for LeafNode

---
+++ src/htmlnode.py
@@ -0,0 +1,13 @@
+class HTMLNode():
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children =  children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        key, value = self.props
+        return f" {key} {value}"
---
+++ src/test_leafnode.py
@@ -0,0 +1,13 @@
+import unittest
+
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "www.google.com"})
+        self.assertNotEqual(node.to_html(), '<a href="www.google.com">Click me!</a>')
